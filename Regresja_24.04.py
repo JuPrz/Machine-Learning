d73611d1c3a74cbf4f2042b115417663a4ed781e
JuPrz <julia.przybyszewska2@wp.pl>
2023-05-08 17:17:40 +0200

    Update 08.05

--- Regresja_24.04.py
+++ Regresja_24.04.py
@@ -1,3 +1,7 @@
+from sklearn import linear_model
+from tabulate import tabulate
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import tree
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -6,34 +10,74 @@ from sklearn.preprocessing import LabelEncoder
 import math
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
-df_x = pd.read_csv("Salary Data.csv" , sep = "," , encoding = 'utf-8')
-print(df_x)
-df_y=df_x['Salary']
-df_x=df_x.drop('Salary')
-# najpierw zmienne kategoryczne na numeryczne
+df = pd.read_csv("Salary Data.csv" , sep = "," , encoding = 'utf-8')
+print(df)
+
+# # Zmiana zmiennych kategorycznych na numeryczne 
+#  Standaryzacja danych numerycznych 
+
+columns = ['Gender', 'Education Level', 'Job Title']
+df[columns] = df[columns].apply(LabelEncoder().fit_transform)
+
+df = df.dropna(inplace=False)
+
+X = df.drop('Salary', axis=1).values
+y = df['Salary'].values
+
 # potem dzielimy zbiór na testowy i treningowy 
-# budujemy model regresji 
-# policzyć regresję liniową 
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
-# Zmiana zmiennych kategorycznych na numeryczne 
-cols = ['Gender', 'Education Level', 'Job Title','Age','Salary']
-# Encode labels of multiple columns at once
-df_x[cols] = df_x[cols].apply(LabelEncoder().fit_transform)
+regr = linear_model.LinearRegression()
 
-print(df_x.head())
+regr.fit(x_train, y_train)
+predictions = regr.predict(x_test)
+print("Regression score:", regr.score(x_test, y_test))
 
+# Lasso regression with alpha=0.1
+lasso = linear_model.Lasso(alpha=0.1)
+lasso.fit(x_train, y_train)
+predictions_lasso = lasso.predict(x_test)
 
+print("Lasso\nR-squared:", lasso.score(x_test, y_test))
+print("MAE:", np.mean(np.abs(predictions_lasso - y_test)))
 
+# Polynomial regression 3rd degree
+poly = PolynomialFeatures(degree=3, include_bias=False)
+x_train_poly = poly.fit_transform(x_train)
+x_test_poly = poly.fit_transform(x_test)
 
-# Standaryzacja danych numerycznych 
-scaler = MinMaxScaler()
-df_cleaned = df_x[cols]
-col_float = ['Gender', 'Education Level', 'Job Title']
-for i in df_cleaned[col_float]:
-    df_cleaned[i] =scaler.fit_transform(df_cleaned[[i]])
-print(df_cleaned.head())
+regr_poly = linear_model.LinearRegression()
+regr_poly.fit(x_train_poly, y_train)
+predictions_poly = regr_poly.predict(x_test_poly)
 
+print("Poly\nR-squared:", regr_poly.score(x_test_poly, y_test))
+print("MAE:", np.mean(np.abs(predictions_poly - y_test)))
 
+# Decision tree
+regr_tree = tree.DecisionTreeRegressor(max_depth=5)
+regr_tree.fit(x_train, y_train)
+predictions_tree = regr_tree.predict(x_test)
 
-# # potem dzielimy zbiór na testowy i treningowy 
-# X_train, X_test, y_train, y_test = train_test_split( df_x,df_y, test_size=0.33, random_state=42)
+print("Regression Tree\nR-squared:", regr_tree.score(x_test, y_test))
+print("MAE:", np.mean(np.abs(predictions_tree - y_test)))
+
+table = [['','Lasso','Polynomial','Tree'],
+         ['R-squared',lasso.score(x_test, y_test),regr_poly.score(x_test_poly, y_test),regr_tree.score(x_test, y_test)],
+         ['MAE',np.mean(np.abs(predictions_lasso - y_test)),np.mean(np.abs(predictions_poly - y_test)),np.mean(np.abs(predictions_tree - y_test))]]
+
+scores = tabulate(table, headers='firstrow')
+with open('score_table.txt', 'w') as f:
+    f.write(scores)
+
+#Wykres z wynikami
+plt.figure(figsize=(10, 5))
+plt.plot(table, scores='Train Score')
+plt.xlabel('Model')
+plt.ylabel('Score')
+plt.title('Model Comparison')
+plt.legend()
+plt.show()
+
+#Tabela wyników
+results = pd.DataFrame({'Model': table,
+                        'Train Score': scores})
